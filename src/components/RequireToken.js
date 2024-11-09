1af0913e80a662c2e756ae5e169054441736af62
Damian Oguche <damianguch@yahoo.com>
2024-11-09 07:33:59 +0000

    Created RequireToken component and refactored the logic to allow access to dashboard after authentication

--- src/components/RequireToken.js
+++ src/components/RequireToken.js
@@ -7,23 +7,33 @@ const RequireToken = ({ children }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Check if the user is authenticated
-    axios
-      .get('https://ladx-backend-ts.onrender.com/api/v1/check-auth', {
-        withCredentials: true
-      })
-      .then((res) => {
-        if (res.data.isAuthenticated) {
-          setIsAuthenticated(true);
-        } else {
+    const checkAuth = () => {
+      // Check if the user is authenticated
+      axios
+        .get('https://ladx-backend-ts.onrender.com/api/v1/check-auth', {
+          withCredentials: true
+        })
+        .then((res) => {
+          if (res.data.isAuthenticated) {
+            setIsAuthenticated(true);
+          } else {
+            setIsAuthenticated(false);
+            navigate('/login'); // Redirect to login if not authenticated
+          }
+        })
+        .catch(() => {
           setIsAuthenticated(false);
-          navigate('/login'); // Redirect to login if not authenticated
-        }
-      })
-      .catch(() => {
-        setIsAuthenticated(false);
-        navigate('/login'); // Redirect to login if any error occurs
-      });
+          navigate('/login'); // Redirect to login if any error occurs
+        });
+    };
+
+    checkAuth();
+
+    // Add listener for login status changes
+    window.addEventListener('authChange', checkAuth);
+
+    // Clean up the event listener
+    return () => window.removeEventListener('authChange', checkAuth);
   }, [navigate]);
 
   // Show loading state until authentication is confirmed
